dc78ce5920fd3e6e65002fe71dee479891f3ebb2
AlekseyDubov <freq731@yandex.ru>
2018-09-30 21:52:34 +0700

    Add files via upload

---
+++ 2lab/parsing population.rb
@@ -0,0 +1,9 @@
+﻿require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(open('https://ru.wikipedia.org/wiki/%D0%91%D0%B5%D1%80%D0%B4%D1%81%D0%BA'))
+
+puts "Население:"
+doc.css('tr td span.no-wikidata span.nowrap', 'article td').each do |link|
+  puts link.content
+end
